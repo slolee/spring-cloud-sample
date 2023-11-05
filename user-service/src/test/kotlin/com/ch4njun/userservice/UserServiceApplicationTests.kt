bd98ba1ffb055f0ab2873995aac7e3c1f98a6ded
slolee <slolee@naver.com>
2023-11-05 20:10:35 +0900

    First Commit

---
+++ user-service/src/test/kotlin/com/ch4njun/userservice/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ch4njun.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
