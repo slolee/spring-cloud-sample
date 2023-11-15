10ba7023225d1ad882c20403c7a5152feaaf8421
slolee <slolee@naver.com>
2023-11-16 01:31:32 +0900

    Feature: API Gateway Sample Filter 추가

---
+++ api-gateway-service/src/main/kotlin/com/ch4njun/apigatewayservice/config/SampleFilterConfig.kt
@@ -0,0 +1,23 @@
+package com.ch4njun.apigatewayservice.config
+
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.filters
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SampleFilterConfig {
+
+    @Bean
+    fun gatewayRoutes(builder: RouteLocatorBuilder) = builder.routes {
+        route {
+            path("/first-service/**")
+            filters {
+                addRequestHeader("first-service-request", "first-request-header")
+                addResponseHeader("first-service-response", "first-response-header")
+            }
+            uri("http://localhost:8081")
+        }
+    }
+}
