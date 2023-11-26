30b54078c0a2a3af8d39d265f183203f0b2f772a
slolee <slolee@naver.com>
2023-11-26 20:03:04 +0900

    Refactor: 프로젝트 구조 헥사고날 아키텍처 기반으로 변경

--- user-service/src/main/kotlin/com/ch4njun/userservice/app/user/HealthCheckController.kt
+++ user-service/src/main/kotlin/com/ch4njun/userservice/app/user/HealthCheckController.kt
@@ -1,4 +1,4 @@
-package com.ch4njun.userservice.controller
+package com.ch4njun.userservice.app.user
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
--- user-service/src/main/kotlin/com/ch4njun/userservice/app/user/controller/UserController.kt
+++ user-service/src/main/kotlin/com/ch4njun/userservice/app/user/controller/UserController.kt
@@ -1,4 +1,4 @@
-package com.ch4njun.userservice.controller
+package com.ch4njun.userservice.app.user.controller
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
